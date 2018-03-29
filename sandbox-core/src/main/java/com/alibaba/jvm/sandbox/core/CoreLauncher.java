619c728c8458706922506f9865ffd07eec790010
feshfans <feshfans@163.com>
2018-03-29 09:44:11 +0800

    去掉无效的类引用

--- sandbox-core/src/main/java/com/alibaba/jvm/sandbox/core/CoreLauncher.java
+++ sandbox-core/src/main/java/com/alibaba/jvm/sandbox/core/CoreLauncher.java
@@ -2,8 +2,6 @@ package com.alibaba.jvm.sandbox.core;
 
 import com.sun.tools.attach.VirtualMachine;
 import org.apache.commons.lang3.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 
 import static com.alibaba.jvm.sandbox.core.util.SandboxStringUtils.getCauseMessage;
