eb05be5b5572bba8f8f1479a1b57a8ead44aebf2
huangxuyang-sz <huangxuyang-sz@58ganji.com>
2019-04-07 15:45:23 +0800

    添加生命周期适配器让用户选择实现指定生命周期方法;Command和Http路径兼容路径/前缀;

--- sandbox-core/src/main/java/com/alibaba/jvm/sandbox/core/server/jetty/servlet/ModuleHttpServlet.java
+++ sandbox-core/src/main/java/com/alibaba/jvm/sandbox/core/server/jetty/servlet/ModuleHttpServlet.java
@@ -32,7 +32,7 @@ import static com.alibaba.jvm.sandbox.api.util.GaStringUtils.matching;
  * Created by luanjia@taobao.com on 2017/2/7.
  */
 public class ModuleHttpServlet extends HttpServlet {
-
+    private static final String SLASH = "/";
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
     private final CoreModuleManager coreModuleManager;
@@ -179,27 +179,37 @@ public class ModuleHttpServlet extends HttpServlet {
             if (null == commandAnnotation) {
                 continue;
             }
-            final String pathOfCmd = "/" + uniqueId + "/" + commandAnnotation.value();
+            // 兼容 value 是否以 / 开头的写法
+            String cmd = appendSlash(commandAnnotation.value());
+            final String pathOfCmd = "/" + uniqueId + cmd;
             if (StringUtils.equals(path, pathOfCmd)) {
                 return method;
             }
         }
-
         // 查找@Http注解的方法
         for (final Method method : MethodUtils.getMethodsListWithAnnotation(classOfModule, Http.class)) {
             final Http httpAnnotation = method.getAnnotation(Http.class);
             if (null == httpAnnotation) {
                 continue;
             }
-            final String pathPattern = "/" + uniqueId + httpAnnotation.value();
+            // 兼容 value 是否以 / 开头的写法
+            String cmd = appendSlash(httpAnnotation.value());
+            final String pathPattern = "/" + uniqueId + cmd;
             if (ArrayUtils.contains(httpAnnotation.method(), httpMethod)
                     && matching(path, pathPattern)) {
                 return method;
             }
         }
-
         // 找不到匹配方法，返回null
         return null;
+    }
+
+    private String appendSlash(String cmd) {
+        // 若不以 / 开头，则添加 /
+        if (!cmd.startsWith(SLASH)) {
+            cmd = SLASH + cmd;
+        }
+        return cmd;
     }
 
     private boolean isMapWithGenericParameterTypes(final Method method,
