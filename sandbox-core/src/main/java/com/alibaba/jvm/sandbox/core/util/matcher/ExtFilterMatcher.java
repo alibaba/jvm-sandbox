d441a1501a850399e08539bde4524b2e7da34fa8
dongchenxu <luanjia.hz@gmail.com>
2019-12-06 14:10:09 +0800

    做非空判断，防止遇到BootstrapClassLoader导致异常

--- sandbox-core/src/main/java/com/alibaba/jvm/sandbox/core/util/matcher/ExtFilterMatcher.java
+++ sandbox-core/src/main/java/com/alibaba/jvm/sandbox/core/util/matcher/ExtFilterMatcher.java
@@ -81,12 +81,13 @@ public class ExtFilterMatcher implements Matcher {
             // 根据 #203 ClassStructureImplByJDK会存在类加载异步的问题
             // 所以这里对JDK实现的ClassStructure抛出NoClassDefFoundError的时候做一个兼容
             // 转换为ASM实现然后进行match
-            if (classStructure instanceof ClassStructureImplByJDK) {
+            if (classStructure instanceof ClassStructureImplByJDK
+                    && classStructure.getClassLoader() != null) {
                 final String javaClassResourceName = toInternalClassName(classStructure.getJavaClassName()).concat(".class");
                 InputStream is = null;
                 try {
                     is = classStructure.getClassLoader().getResourceAsStream(javaClassResourceName);
-                    return _matching(ClassStructureFactory.createClassStructure(is, classStructure.getClassLoader()));
+                    _matching(ClassStructureFactory.createClassStructure(is, classStructure.getClassLoader()));
                 } finally {
                     IOUtils.closeQuietly(is);
                 }
